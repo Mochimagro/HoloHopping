81ec42ecd2580744b8bd1d72b6a10ec74df74677
mochimagro <tasklog2@outlook.jp>
2021-01-17 14:54:02 +0900

    ジャンプする方向の実装

---
+++ Assets/_MainGame/_Scripts/Component/MainPlayerComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace ProjectName.MainPlayerComponent
+{
+
+    public class MainPlayerComponent : MonoBehaviour
+    {        
+        
+    }
+}
