61d287f55abf0528151c837542406b1afec6fb3a
mochimagro <tasklog2@outlook.jp>
2021-01-25 21:20:50 +0900

    MVPパターンのテンプレート化拡張機能を導入(自作やぞ)

---
+++ Assets/Editor/ScriptTemplateCreater/CreateAssetDataEntity.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+
+namespace MochimagroEditor.CreateAsset
+{
+    public class CreateAssetDataEntity : EndNameEditAction
+    {
+        [MenuItem("Assets/Create/ScriptTemplate/DataAndEntity", false, -1)]
+        private static void CreateMonoBehaviour()
+        {
+            var resourceFile = Path.Combine(
+                Application.dataPath,
+                "Editor/ScriptTemplateCreater/ScriptTemplates/Data.cs.txt");
+
+
+            // unityで用意されているC#のアイコンを利用する
+            var csIcon =
+                EditorGUIUtility.IconContent("cs Script Icon").image as Texture2D;
+
+            // ScriptableObjectのインスタンスとして作成する
+            var endNameEditAction =
+                ScriptableObject.CreateInstance<CreateAssetDataEntity>();
+
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(
+                0,
+                endNameEditAction,
+                "NewData.cs",
+                csIcon,
+                resourceFile);
+
+        }
+
+        public override void Action(int instanceId, string pathName, string resourceFile)
+        {
+
+            var text = File.ReadAllText(resourceFile);
+            var pathes = Application.dataPath.Split('/');
+
+            var name = Path.GetFileNameWithoutExtension(pathName);
+            var scriptName = name.Replace(" ", "");
+            var projectName = pathes[pathes.Length - 2];
+            projectName = projectName.Replace(" ", "");
+            var directryName = Path.GetDirectoryName(pathName).
+                                Replace("Assets", "").
+                                Replace("/Scripts", "").
+                                Replace("/", ".");
+
+            text = text.Replace("#NAME#", name);
+            text = text.Replace("#SCRIPTNAME#", scriptName);
+            text = text.Replace("#PROJECTNAME#", projectName);
+            text = text.Replace("#DIRECTORYNAME#", directryName);
+            text = text.Replace("#NOTRIM#", "\n");
+
+            var encording = new UTF8Encoding(true, false);
+
+            File.WriteAllText(pathName, text, encording);
+
+            AssetDatabase.ImportAsset(pathName);
+            var asset = AssetDatabase.LoadAssetAtPath<MonoScript>(pathName);
+
+            ProjectWindowUtil.ShowCreatedAsset(asset);
+        }
+    }
+}
---
+++ Assets/Editor/ScriptTemplateCreater/CreateAssetMVP.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+
+namespace MochimagroEditor.CreateAsset
+{
+    public class CreateAssetMVP : EndNameEditAction
+    {
+        [MenuItem("Assets/Create/ScriptTemplate/MVP", false, -1)]
+        private static void CreateMonoBehaviour()
+        {
+            // unityで用意されているC#のアイコンを利用する
+            var groupIcon =
+                EditorGUIUtility.IconContent("TreeEditor.Duplicate").image as Texture2D;
+
+            // ScriptableObjectのインスタンスとして作成する
+            var endNameEditAction =
+                ScriptableObject.CreateInstance<CreateAssetMVP>();
+
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(
+                0,
+                endNameEditAction,
+                "NewMVPGroup",
+                groupIcon,
+                string.Empty);
+
+        }
+
+        public override void Action(int instanceId, string pathName, string resourceFile)
+        {
+            string[] fileType = new string[] { "Model", "View", "Presenter" };
+
+            var groupName = Path.GetFileNameWithoutExtension(pathName);
+            var pathes = pathName.Split('/');
+
+            // ディレクトリの作成
+            var directoryPath = pathName.Replace(pathes[pathes.Length - 1], groupName);
+            Directory.CreateDirectory(directoryPath);
+
+            foreach (var type in fileType)
+            {
+                var resourceModelFile = Path.Combine(
+                    Application.dataPath,
+                    "Editor/ScriptTemplateCreater/ScriptTemplates/" + type + ".cs.txt");
+                CreateScript(directoryPath, resourceModelFile, type);
+            }
+
+            return;
+
+        }
+
+        private void CreateScript(string pathName, string resourceFile, string fileType)
+        {
+            var text = File.ReadAllText(resourceFile);
+            var pathes = Application.dataPath.Split('/');
+
+            var groupName = Path.GetFileNameWithoutExtension(pathName);
+            var projectName = pathes[pathes.Length - 2];
+            projectName = projectName.Replace(" ", "");
+
+            text = text.Replace("#NAME#", groupName);
+            text = text.Replace("#NAMETOLOWER#", char.ToLower(groupName[0]) + groupName.Substring(1));
+            text = text.Replace("#PROJECTNAME#", projectName);
+            text = text.Replace("#NOTRIM#", "\n");
+
+            var encording = new UTF8Encoding(true, false);
+
+            var filePath = pathName + "/" + groupName + fileType + ".cs";
+
+            File.WriteAllText(filePath, text, encording);
+
+            AssetDatabase.ImportAsset(filePath);
+            var asset = AssetDatabase.LoadAssetAtPath<MonoScript>(filePath);
+
+            ProjectWindowUtil.ShowCreatedAsset(asset);
+
+
+        }
+    }
+
+}
