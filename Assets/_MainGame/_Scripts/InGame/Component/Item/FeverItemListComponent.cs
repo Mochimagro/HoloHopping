c47eef157188ec48a7dcbaff17bbc2626c7f3bc8
mochimagro <tasklog2@outlook.jp>
2021-02-07 04:38:36 +0900

    フィーバーモードの生成システム実装

---
+++ Assets/_MainGame/_Scripts/InGame/Component/Item/FeverItemListComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace HoloHopping.Component
+{
+
+    public class FeverItemListComponent : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _positions = new List<GameObject>();
+
+        public List<GameObject> Positions { get => _positions; }
+
+    }
+}
---
+++ Assets/_MainGame/_Scripts/InGame/Component/Item/FeverPatternListComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace HoloHopping.Component
+{
+
+    public class FeverPatternListComponent : MonoBehaviour
+    {
+        [SerializeField] private List<FeverItemListComponent> _sets = new List<FeverItemListComponent>();
+        public List<Vector3> GetRandomPattern
+        {
+            get
+            {
+                var target = _sets[UnityEngine.Random.Range(0, _sets.Count)];
+                return target.Positions.Select(go => go.transform.position).ToList();
+            }
+        }
+    }
+}
